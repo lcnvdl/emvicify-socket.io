3ad94aba18edc9383983b84e1010405368ba4607
Lcnvdl <programando.soft@gmail.com>
2020-09-08 02:14:22 -0300

    Update

--- src/driver.js
+++ src/driver.js
@@ -1,10 +1,20 @@
 const { drivers } = require("@emvicify/base")
-const { WebSocketsDriver } = drivers;
+const WebSocketsDriver = drivers.websockets;
+
+// var app = require('express')();
+// var http = require('http').createServer(app);
+// var io = require('socket.io')(http);
+
+const Server = require('socket.io');
 
 class Driver extends WebSocketsDriver {
     constructor() {
         super();
+        this.port = null;
+        this.expressApp = null;
+        this.httpServer = null;
         this.isRunning = false;
+        this.__server = null;
     }
 
     get info() {
@@ -16,7 +26,25 @@ class Driver extends WebSocketsDriver {
         }
     }
 
-    listen() {
+    listen(port, opts) {
+        const io = new Server(port || this.port || this.httpServer, opts);
+
+        io.on('connection', (socket) => {
+            const connection = this.createConnection(socket);
+
+            this.events.emit(this.socketEvent.Connect, connection);
+
+            socket.on("message", data => {
+                this.events.emit(this.socketEvent.Message, connection);
+            });
+
+            socket.on("disconnect", () => {
+                this.events.emit(this.socketEvent.Disconnect, connection);
+            });
+        });
+
+        this.__server = io;
+        this.isRunning = true;
     }
 }
 
--- src/plugin.js
+++ src/plugin.js
@@ -6,8 +6,20 @@ module.exports = {
     description: "Websockets implementation using Socket.IO",
     version: "0.0.1",
     events: {
-        configureAppBeforeServe({app, http, modules}) {
+        configureAppBeforeServe({ app, http, modules }) {
+            const settings = modules.settings["socket-io"];
+
+            if (settings) {
+                driver.port = settings.port || driver.port;
+            }
+
+            driver.expressApp = app;
+            driver.httpServer = http;
             modules.drivers[driver.type] = driver;
+        },
+
+        appStarted({ app, http, modules }) {
+            driver.listen();
         }
     }
 };
